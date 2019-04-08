5709e2f8a1cca42aad9cc9637164070368d575cd
Luis Guilherme de Souza <luis.guilherme@sysmap.com.br>
2019-04-08 20:15:17 -0300

    Commit 1: Form do frontend da aplicação

---
+++ code-compare/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+import Style from "./styles/global";
+
+import Main from "./pages/Main";
+
+const App = () => (
+  <Fragment>
+    <Style />
+    <Main />
+  </Fragment>
+);
+
+export default App;
